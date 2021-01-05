461316ef08fbc1d3b184fb6b999c6c4166996993
desutedja <de.sutedja@gmail.com>
2021-01-05 14:36:33 +0700

    "minor"

--- src/App.vue
+++ src/App.vue
@@ -34,17 +34,17 @@
             <div class="row">
               <div class="footer-col col-md-4">
                 <h3>Location</h3>
-                <p>3481 Melrose Place<br>Beverly Hills, CA 90210</p>
+                <p>Indonesia<br>Jakarta</p>
               </div>
               <div class="footer-col col-md-4">
-                <h3>Around the Web</h3>
+                <h3>Around the World</h3>
                 <ul class="list-inline">
                   <!-- some social networks -->
                 </ul>
               </div>
               <div class="footer-col col-md-4">
                 <h3>About Us</h3>
-                <p>its is a free to use, open source Bootstrap theme created by Start Bootstrap.</p>
+                <p>its all about us</p>
               </div>
             </div>
           </div>
@@ -53,7 +53,7 @@
           <div class="container">
             <div class="row">
               <div class="col-lg-12">
-                Author
+                Thanks You!
               </div>
             </div>
           </div>
